c2fa4e4f57ce1ffd00866f44ce86232c768e458d
Vijay sahu <vijaysahu4914@gmail.com>
2024-09-23 17:47:14 +0530

    add importmap

---
+++ config/importmap.rb
@@ -0,0 +1,107 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application" # @0.1.4
+pin "#util.inspect.js" # @2.0.1
+pin "array-buffer-byte-length" # @1.0.1
+pin "assert" # @2.0.1
+pin "available-typed-arrays" # @1.0.7
+pin "bindings" # @1.5.0
+pin "buffer" # @2.0.1
+pin "call-bind" # @1.0.7
+pin "call-bind/callBound", to: "call-bind--callBound.js" # @1.0.7
+pin "child_process" # @2.0.1
+pin "cookies" # @0.3.8
+pin "crypto" # @2.0.1
+pin "define-data-property" # @1.1.4
+pin "define-properties" # @1.2.1
+pin "dnode" # @0.9.12
+pin "dnode-protocol" # @0.1.5
+pin "ejs" # @0.7.2
+pin "error-page" # @0.0.3
+pin "es-abstract/2024/Get", to: "es-abstract--2024--Get.js" # @1.23.3
+pin "es-abstract/2024/GetValueFromBuffer", to: "es-abstract--2024--GetValueFromBuffer.js" # @1.23.3
+pin "es-abstract/2024/IsDetachedBuffer", to: "es-abstract--2024--IsDetachedBuffer.js" # @1.23.3
+pin "es-abstract/2024/Set", to: "es-abstract--2024--Set.js" # @1.23.3
+pin "es-abstract/2024/SetValueInBuffer", to: "es-abstract--2024--SetValueInBuffer.js" # @1.23.3
+pin "es-abstract/2024/ToIntegerOrInfinity", to: "es-abstract--2024--ToIntegerOrInfinity.js" # @1.23.3
+pin "es-abstract/2024/ToString", to: "es-abstract--2024--ToString.js" # @1.23.3
+pin "es-abstract/2024/TypedArrayElementSize", to: "es-abstract--2024--TypedArrayElementSize.js" # @1.23.3
+pin "es-abstract/2024/TypedArrayElementType", to: "es-abstract--2024--TypedArrayElementType.js" # @1.23.3
+pin "es-abstract/2024/TypedArraySpeciesCreate", to: "es-abstract--2024--TypedArraySpeciesCreate.js" # @1.23.3
+pin "es-abstract/2024/ValidateTypedArray", to: "es-abstract--2024--ValidateTypedArray.js" # @1.23.3
+pin "es-abstract/2024/max", to: "es-abstract--2024--max.js" # @1.23.3
+pin "es-abstract/2024/min", to: "es-abstract--2024--min.js" # @1.23.3
+pin "es-abstract/helpers/getProto", to: "es-abstract--helpers--getProto.js" # @1.23.3
+pin "es-define-property" # @1.0.0
+pin "es-errors" # @1.3.0
+pin "es-errors/eval", to: "es-errors--eval.js" # @1.3.0
+pin "es-errors/range", to: "es-errors--range.js" # @1.3.0
+pin "es-errors/ref", to: "es-errors--ref.js" # @1.3.0
+pin "es-errors/syntax", to: "es-errors--syntax.js" # @1.3.0
+pin "es-errors/type", to: "es-errors--type.js" # @1.3.0
+pin "es-errors/uri", to: "es-errors--uri.js" # @1.3.0
+pin "es-object-atoms/RequireObjectCoercible", to: "es-object-atoms--RequireObjectCoercible.js" # @1.0.0
+pin "es-to-primitive/es2015", to: "es-to-primitive--es2015.js" # @1.2.1
+pin "events" # @2.0.1
+pin "file-uri-to-path" # @1.0.0
+pin "for-each" # @0.3.3
+pin "fs" # @2.0.1
+pin "function-bind" # @1.1.2
+pin "get-intrinsic" # @1.2.4
+pin "gopd" # @1.0.1
+pin "has-property-descriptors" # @1.0.2
+pin "has-proto" # @1.0.3
+pin "has-symbols" # @1.0.3
+pin "has-symbols/shams", to: "has-symbols--shams.js" # @1.0.3
+pin "has-tostringtag/shams", to: "has-tostringtag--shams.js" # @1.0.2
+pin "hasown" # @2.0.2
+pin "hiredis" # @0.5.0
+pin "http" # @2.0.1
+pin "https" # @2.0.1
+pin "is-array-buffer" # @3.0.4
+pin "is-callable" # @1.2.7
+pin "is-date-object" # @1.0.5
+pin "is-regex" # @1.1.3
+pin "is-shared-array-buffer" # @1.0.3
+pin "is-symbol" # @1.0.4
+pin "is-typed-array" # @1.1.13
+pin "isarray" # @2.0.5
+pin "jsonify" # @0.0.1
+pin "keygrip" # @0.2.4
+pin "lazy" # @1.0.11
+pin "lru-cache" # @1.1.1
+pin "msgpack" # @1.0.3
+pin "net" # @2.0.1
+pin "node-static" # @0.5.9
+pin "object-inspect" # @1.13.2
+pin "object-keys" # @1.1.1
+pin "path" # @2.0.1
+pin "policyfile" # @0.0.4
+pin "possible-typed-array-names" # @1.0.0
+pin "process" # @2.0.1
+pin "querystring" # @2.0.1
+pin "redis" # @0.7.3
+pin "redsess" # @0.0.6
+pin "safe-array-concat" # @1.1.2
+pin "safe-regex-test" # @1.0.3
+pin "set-function-length" # @1.2.2
+pin "socket.io" # @0.8.6
+pin "socket.io-client" # @0.8.6
+pin "stream" # @2.0.1
+pin "string.prototype.trim" # @1.2.9
+pin "sys" # @2.0.1
+pin "templar" # @0.0.5
+pin "tls" # @2.0.1
+pin "traverse" # @0.6.10
+pin "typed-array-buffer" # @1.0.2
+pin "typed-array-byte-offset" # @1.0.2
+pin "typed-array-length" # @1.0.6
+pin "typedarray.prototype.slice" # @1.0.3
+pin "uglify-js" # @1.0.6
+pin "url" # @2.0.1
+pin "util" # @2.0.1
+pin "which-typed-array" # @1.1.15
+pin "xmlhttprequest" # @1.2.2
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.10
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.10
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.2.100
