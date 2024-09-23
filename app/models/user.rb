4182ff72d23bf8b37077f12560dc64710f5d21ca
Vijay sahu <vijaysahu4914@gmail.com>
2024-09-23 23:44:23 +0530

    manage random passowrd and user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { admin: 0, super_master: 1, agent: 2, client: 3 }
+  validates :user_name, presence: true, uniqueness: true
+
+  def random_password
+    chars = ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
+    Array.new(6) { chars.sample }.join
+  end
+
+  def random_generate_user_name(role_type)
+    last_number = User.last.user_name.present? ? User.last.user_name[-5..-1].to_i : 0
+    prefix = case role_type
+              when 'super_admin'
+                'SUP'
+              when 'agent'
+                'AGT'
+              when 'client'
+                'CLT'
+             else
+               'USR'
+             end
+    sequence_number = (last_number + 1).to_s.rjust(5, '0')
+    
+    "#{prefix}#{sequence_number}"
+  end
 end
